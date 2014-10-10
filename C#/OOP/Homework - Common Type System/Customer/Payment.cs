5767da4817f788e4ca734e4833f389046882ce53
alex687 <alex.aramov@gmail.com>
2014-10-10 23:47:00 +0300

    Homework CTS not finished

---
+++ C#/OOP/Homework - Common Type System/Customer/Payment.cs
@@ -0,0 +1,65 @@
+﻿namespace Customer
+{
+    using System;
+
+    class Payment
+    {
+        private int price;
+        private string productName;
+
+        public Payment(string productName, int price)
+        {
+            this.ProductName = productName;
+            this.Price = price;
+        }
+
+        public string ProductName
+        {
+            get
+            {
+                return this.productName;
+                
+            }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Product name cannot be null or empty");
+                }
+
+                this.productName = value;
+            }
+        }
+
+        public int Price
+        {
+            get
+            {
+                return this.price;
+                
+            }
+
+            set
+            {
+                if(value <= 0 )
+                {
+                    throw new ArgumentException("Price cannot be < 0");
+                }
+
+                this.price = value;
+            }
+        }
+
+        public object Clone()
+        {
+            Payment newPayment = this.MemberwiseClone() as Payment;
+            if (null == newPayment)
+            {
+                throw new ArgumentNullException("Cloned object can not be casted to type Payment!");
+            }
+
+            return newPayment;
+        }
+    }
+}
---
+++ C#/OOP/Homework - Common Type System/Customer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer @new = new Customer(9502030405, "hasan", "hasan", "hasan", "0884899549", "hasan@abv.bg", "sadasd",
+                CustomerType.Diamond, new List<Payment>());
+        }
+    }
+}
