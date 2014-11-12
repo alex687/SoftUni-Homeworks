a8f25b86594fef2645649a82210c6b7f2a82f2d1
alex687 <alex.aramov@gmail.com>
2014-11-12 17:12:46 +0200

    JS OOP Exam prep

--- JavaScript OOP Homework/ExamPrep/Estates/Estates.js
+++ JavaScript OOP Homework/ExamPrep/Estates/Estates.js
@@ -2,53 +2,346 @@
 
     'use strict';
 
-
-    var Estate = function() {
-        // TODO: define the missing class 
+    Object.prototype.inherits = function inherits(parent) {
+        this.prototype = Object.create(parent.prototype);
+        this.prototype.constructor = this;
     };
 
-
-    var BuildingEstate = function() {
-        // TODO: define the missing class 
+    Object.prototype.validateNonEmptyString = function (value, variableName) {
+        if (typeof (value) !== 'string' || !value) {
+            throw new Error(variableName + " should be non-empty string.");
+        }
     };
 
-
-    var Apartment = function() {
-        // TODO: define the missing class 
+    Object.prototype.validateIntegerRange = function (value, variableName, minValue, maxValue) {
+        if (typeof (value) !== 'number') {
+            throw new Error(variableName + " should be a number.");
+        }
+        if (value !== parseInt(value, 10)) {
+            throw new Error(variableName + " should be integer.");
+        }
+        if (value < minValue || value > maxValue) {
+            throw new Error(variableName + " should be integer in the range [" +
+            minValue + "..." + maxValue + "].");
+        }
     };
 
-
-    var Office = function() {
-        // TODO: define the missing class 
+    Object.prototype.validateBoolean = function (value, variableName) {
+        if (typeof (value) !== 'boolean') {
+            throw new Error(variableName + " should be a boolean value.");
+        }
     };
 
-
-    var House = function() {
-        // TODO: define the missing class 
+    Object.prototype.validateNonEmptyObject = function (value, className, variableName) {
+        if (!(value instanceof className)) {
+            throw new Error(variableName + " should be non-empty " +
+            className.prototype.constructor.name + ".");
+        }
     };
 
+    var Estate = (function () {
 
-    var Garage = function() {
-        // TODO: define the missing class 
-    };
+        function Estate(name, area, location, isFurnitured) {
+            if (this.constructor === Estate) {
+                throw new Error("Can't instantiate abstract class Estate.");
+            }
+
+            this.setName(name);
+            this.setArea(area);
+            this.setLocation(location);
+            this._setFurniture(isFurnitured);
+        }
+
+        // Get Name
+        Estate.prototype.getName = function () {
+            return this._name;
+        };
+
+        // Set Name
+        Estate.prototype.setName = function (name) {
+            this.validateNonEmptyString(name, "name");
+
+            this._name = name;
+        };
+
+        // Get Area
+        Estate.prototype.getArea = function () {
+            return this._area;
+        };
+
+        // Set Area
+        Estate.prototype.setArea = function (area) {
+            this.validateIntegerRange(area, "area", 1, 10000);
+
+            this._area = area;
+        };
+
+        // Get Location
+        Estate.prototype.getLocation = function () {
+            return this._location;
+        };
+
+        // Set Location
+        Estate.prototype.setLocation = function (location) {
+            this.validateNonEmptyString(location, "location");
+
+            this._location = location;
+        };
+
+        // Get Furniture
+        Estate.prototype.hasFurniture = function () {
+            return this._isFurnitured;
+        };
+
+        // Set Furniture
+        Estate.prototype._setFurniture = function (isFurnitured) {
+            this.validateBoolean(isFurnitured, "isFurnitured");
+
+            this._isFurnitured = isFurnitured;
+        };
+
+        Estate.prototype.toString = function () {
+            var isFurnituredString = this.hasFurniture() ? "Yes" : "No";
+
+            return "Name = " + this.getName() + ", Area = " + this.getArea() + ", Location = " + this.getLocation() + ", Furnitured = " + isFurnituredString;
+        };
+
+        return Estate;
+    }());
 
 
-    var Offer = function() {
-        // TODO: define the missing class 
-    };
+    var BuildingEstate = (function () {
+
+        function BuildingEstate(name, area, location, isFurnitured, numberOfRooms, hasElevator) {
+            if (this.constructor === BuildingEstate) {
+                throw new Error("Can't instantiate abstract class BuildingEstate.");
+            }
+
+            Estate.call(this, name, area, location, isFurnitured);
+
+            this.setNumberOfRooms(numberOfRooms);
+            this.setHasElevator(hasElevator);
+        }
+
+        BuildingEstate.inherits(Estate);
+
+        // Get NumberOfRooms
+        BuildingEstate.prototype.getNumberOfRooms = function () {
+            return this._numberOfRooms;
+        };
+
+        // Set numberOfRooms
+        BuildingEstate.prototype.setNumberOfRooms = function (numberOfRooms) {
+            this.validateIntegerRange(numberOfRooms, "numberOfRooms", 0, 100);
+
+            this._numberOfRooms = numberOfRooms;
+        };
+
+        // Get HasElevator
+        BuildingEstate.prototype.getHasElevator = function () {
+            return this._hasElevator;
+        };
+
+        // Set HasElevator
+        BuildingEstate.prototype.setHasElevator = function (hasElevator) {
+            this.validateBoolean(hasElevator, "hasElevator");
+
+            this._hasElevator = hasElevator;
+        };
+
+        BuildingEstate.prototype.toString = function () {
+            var baseString = Estate.prototype.toString.call(this);
+            var hasElevatorStirng = this.getHasElevator() ? "Yes" : "No";
+
+            return baseString + ", Rooms: " + this.getNumberOfRooms() + ", Elevator: " + hasElevatorStirng;
+        };
+
+        return BuildingEstate;
+    }());
 
 
-    var RentOffer = function() {
-        // TODO: define the missing class 
-    };
+    var Apartment = (function () {
+
+        function Apartment(name, area, location, isFurnitured, numberOfRooms, hasElevator) {
+            BuildingEstate.call(this, name, area, location, isFurnitured, numberOfRooms, hasElevator);
+        }
+
+        Apartment.inherits(BuildingEstate);
+
+        Apartment.prototype.toString = function () {
+            return "Apartment: " + BuildingEstate.prototype.toString.call(this);
+        };
+
+        return Apartment;
+    }());
 
 
-    var SaleOffer = function() {
-        // TODO: define the missing class 
-    };
+    var Office = (function () {
+
+        function Office(name, area, location, isFurnitured, numberOfRooms, hasElevator) {
+            BuildingEstate.call(this, name, area, location, isFurnitured, numberOfRooms, hasElevator);
+        }
+
+        Office.inherits(BuildingEstate);
+
+        Office.prototype.toString = function () {
+            return "Office: " + BuildingEstate.prototype.toString.call(this);
+        };
+
+        return Office;
+    }());
 
 
-    var EstatesEngine = (function() {
+    var House = (function () {
+
+        function House(name, area, location, isFurnitured, floors) {
+            Estate.call(this, name, area, location, isFurnitured);
+
+            this.setFloors(floors);
+        }
+
+        House.inherits(Estate);
+
+        // Get Floors
+        House.prototype.getFloors = function () {
+            return this._floors;
+        };
+
+        // Set Floors
+        House.prototype.setFloors = function (floors) {
+            this.validateIntegerRange(floors, "floors", 1, 10);
+
+            this._floors = floors;
+        };
+
+        House.prototype.toString = function () {
+            var baseString = Estate.prototype.toString.call(this);
+
+            return "House: " + baseString + ", Floors: " + this.getFloors();
+        };
+
+        return House;
+    }());
+
+    var Garage = (function () {
+
+        function Garage(name, area, location, isFurnitured, width, height) {
+            Estate.call(this, name, area, location, isFurnitured);
+
+            this.setWidth(width);
+            this.setHeight(height);
+        }
+
+        Garage.inherits(Estate);
+
+        // Get Width
+        Garage.prototype.getWidth = function () {
+            return this._width;
+        };
+
+        // Set Width
+        Garage.prototype.setWidth = function (width) {
+            this.validateIntegerRange(width, "width", 1, 500);
+            this._width = width;
+        };
+
+        // Get Height
+        Garage.prototype.getHeight = function () {
+            return this._height;
+        };
+
+        // Set Height
+        Garage.prototype.setHeight = function (height) {
+            this.validateIntegerRange(height, "height", 1, 500);
+
+            this._height = height;
+        };
+
+
+        Garage.prototype.toString = function () {
+            var baseString = Estate.prototype.toString.call(this);
+
+            return "Garage: " + baseString + ", Width: " + this.getWidth() + ", Height: " + this.getHeight();
+        };
+
+        return Garage;
+    }());
+
+
+    var Offer = (function () {
+
+        function Offer(estate, price) {
+            this.setEstate(estate);
+            this.setPrice(price);
+        }
+
+        // Get Price
+        Offer.prototype.getPrice = function () {
+            return this._price;
+        };
+
+        // Set Price
+        Offer.prototype.setPrice = function (price) {
+            this.validateIntegerRange(price, "price", 1, Number.MAX_VALUE);
+
+            this._price = price;
+        };
+
+        // Get Estate
+        Offer.prototype.getEstate = function () {
+            return this._estate;
+        };
+
+        // Set Estate
+        Offer.prototype.setEstate = function (estate) {
+            this.validateNonEmptyObject(estate, Estate, "estate");
+            this._estate = estate;
+        };
+
+        Offer.prototype.toString = function () {
+            return "Estate = " + this.getEstate().getName() + ", Location = " + this.getEstate().getLocation() + ", Price = " + this.getPrice();
+        };
+
+        return Offer;
+    }());
+
+
+    var RentOffer = (function () {
+
+        function RentOffer(estate, price) {
+            Offer.call(this, estate, price);
+        }
+
+        RentOffer.inherits(Offer);
+
+        RentOffer.prototype.toString = function () {
+            var baseString = Offer.prototype.toString.call(this);
+
+            return "Rent: " + baseString;
+        };
+        return RentOffer;
+    }());
+
+
+    var SaleOffer = (function () {
+
+        function SaleOffer(estate, price) {
+            Offer.call(this, estate, price);
+        }
+
+        SaleOffer.inherits(Offer);
+
+        SaleOffer.prototype.toString = function () {
+            var baseString = Offer.prototype.toString.call(this);
+
+            return "Sale: " + baseString;
+        };
+
+        return SaleOffer;
+    }());
+
+
+    var EstatesEngine = (function () {
         var _estates;
         var _uniqueEstateNames;
         var _offers;
@@ -64,64 +357,68 @@
             var cmdName = cmdParts[0];
             var cmdArgs = cmdParts.splice(1);
             switch (cmdName) {
-            case 'create':
-                return executeCreateCommand(cmdArgs);
-            case 'status':
-                return executeStatusCommand();
-            case 'find-sales-by-location':
-                return executeFindSalesByLocationCommand(cmdArgs[0]);
-            default:
-                throw new Error('Unknown command: ' + cmdName);
+                case 'create':
+                    return executeCreateCommand(cmdArgs);
+                case 'status':
+                    return executeStatusCommand();
+                case 'find-sales-by-location':
+                    return executeFindSalesByLocationCommand(cmdArgs[0]);
+                case 'find-rents-by-location':
+                    return executeFindRentsByLocationCommand(cmdArgs[0]);
+                case 'find-rents-by-price':
+                    return executeFindRentsByPriceCommand(cmdArgs[0], cmdArgs[1]);
+                default:
+                    throw new Error('Unknown command: ' + cmdName);
             }
         }
 
         function executeCreateCommand(cmdArgs) {
             var objType = cmdArgs[0];
             switch (objType) {
-            case 'Apartment':
-                var apartment = new Apartment(cmdArgs[1], Number(cmdArgs[2]), cmdArgs[3],
-                    parseBoolean(cmdArgs[4]), Number(cmdArgs[5]), parseBoolean(cmdArgs[6]));
-                addEstate(apartment);
-                break;
-            case 'Office':
-                var office = new Office(cmdArgs[1], Number(cmdArgs[2]), cmdArgs[3],
-                    parseBoolean(cmdArgs[4]), Number(cmdArgs[5]), parseBoolean(cmdArgs[6]));
-                addEstate(office);
-                break;
-            case 'House':
-                var house = new House(cmdArgs[1], Number(cmdArgs[2]), cmdArgs[3],
-                    parseBoolean(cmdArgs[4]), Number(cmdArgs[5]));
-                addEstate(house);
-                break;
-            case 'Garage':
-                var garage = new Garage(cmdArgs[1], Number(cmdArgs[2]), cmdArgs[3],
-                    parseBoolean(cmdArgs[4]), Number(cmdArgs[5]), Number(cmdArgs[6]));
-                addEstate(garage);
-                break;
-            case 'RentOffer':
-                var estate = findEstateByName(cmdArgs[1]);
-                var rentOffer = new RentOffer(estate, Number(cmdArgs[2]));
-                addOffer(rentOffer);
-                break;
-            case 'SaleOffer':
-                estate = findEstateByName(cmdArgs[1]);
-                var saleOffer = new SaleOffer(estate, Number(cmdArgs[2]));
-                addOffer(saleOffer);
-                break;
-            default:
-                throw new Error('Unknown object to create: ' + objType);
+                case 'Apartment':
+                    var apartment = new Apartment(cmdArgs[1], Number(cmdArgs[2]), cmdArgs[3],
+                        parseBoolean(cmdArgs[4]), Number(cmdArgs[5]), parseBoolean(cmdArgs[6]));
+                    addEstate(apartment);
+                    break;
+                case 'Office':
+                    var office = new Office(cmdArgs[1], Number(cmdArgs[2]), cmdArgs[3],
+                        parseBoolean(cmdArgs[4]), Number(cmdArgs[5]), parseBoolean(cmdArgs[6]));
+                    addEstate(office);
+                    break;
+                case 'House':
+                    var house = new House(cmdArgs[1], Number(cmdArgs[2]), cmdArgs[3],
+                        parseBoolean(cmdArgs[4]), Number(cmdArgs[5]));
+                    addEstate(house);
+                    break;
+                case 'Garage':
+                    var garage = new Garage(cmdArgs[1], Number(cmdArgs[2]), cmdArgs[3],
+                        parseBoolean(cmdArgs[4]), Number(cmdArgs[5]), Number(cmdArgs[6]));
+                    addEstate(garage);
+                    break;
+                case 'RentOffer':
+                    var estate = findEstateByName(cmdArgs[1]);
+                    var rentOffer = new RentOffer(estate, Number(cmdArgs[2]));
+                    addOffer(rentOffer);
+                    break;
+                case 'SaleOffer':
+                    estate = findEstateByName(cmdArgs[1]);
+                    var saleOffer = new SaleOffer(estate, Number(cmdArgs[2]));
+                    addOffer(saleOffer);
+                    break;
+                default:
+                    throw new Error('Unknown object to create: ' + objType);
             }
             return objType + ' created.';
         }
 
         function parseBoolean(value) {
             switch (value) {
-            case "true":
-                return true;
-            case "false":
-                return false;
-            default:
-                throw new Error("Invalid boolean value: " + value);
+                case "true":
+                    return true;
+                case "false":
+                    return false;
+                default:
+                    throw new Error("Invalid boolean value: " + value);
             }
         }
 
@@ -170,15 +467,44 @@
         }
 
         function executeFindSalesByLocationCommand(location) {
+            return executeFindOfferTypeByLocationCommand(location, SaleOffer);
+        }
+
+        function executeFindRentsByLocationCommand(location) {
+            return executeFindOfferTypeByLocationCommand(location, RentOffer);
+        }
+
+        function executeFindOfferTypeByLocationCommand(location, offerType) {
             if (!location) {
                 throw new Error("Location cannot be empty.");
             }
-            var selectedOffers = _offers.filter(function(offer) {
+            var selectedOffers = _offers.filter(function (offer) {
                 return offer.getEstate().getLocation() === location &&
-                    offer instanceof SaleOffer;
+                    offer instanceof offerType;
             });
-            selectedOffers.sort(function(a, b) {
+            selectedOffers.sort(function (a, b) {
                 return a.getEstate().getName().localeCompare(b.getEstate().getName());
+            });
+            return formatQueryResults(selectedOffers);
+        }
+
+        function executeFindRentsByPriceCommand(minPrice, maxPrice) {
+            minPrice = Number(minPrice);
+            maxPrice = Number(maxPrice);
+
+            Object.validateIntegerRange(maxPrice, "maxPrice", minPrice, Math.MAX_VALUE);
+            Object.validateIntegerRange(minPrice, "minPrice", 0, maxPrice);
+
+            var selectedOffers = _offers.filter(function (offer) {
+                return offer.getPrice() >= minPrice && offer.getPrice() <= maxPrice &&
+                    offer instanceof RentOffer;
+            });
+            selectedOffers.sort(function (a, b) {
+                var result = a.getPrice() - b.getPrice();
+                if (result == 0) {
+                    result = a.getEstate().getName().localeCompare(b.getEstate().getName());
+                }
+                return result;
             });
             return formatQueryResults(selectedOffers);
         }
@@ -192,8 +518,8 @@
                 for (var i = 0; i < offers.length; i++) {
                     var offer = offers[i];
                     result += '  [Estate: ' + offer.getEstate().getName() +
-                        ', Location: ' + offer.getEstate().getLocation() +
-                        ', Price: ' + offer.getPrice() + ']\n';
+                    ', Location: ' + offer.getEstate().getLocation() +
+                    ', Price: ' + offer.getPrice() + ']\n';
                 }
             }
             return result.trim();
@@ -209,7 +535,7 @@
     // Process the input commands and return the results
     var results = '';
     EstatesEngine.initialize();
-    commands.forEach(function(cmd) {
+    commands.forEach(function (cmd) {
         if (cmd != '') {
             try {
                 var cmdResult = EstatesEngine.executeCommand(cmd);
@@ -229,16 +555,16 @@
 // Remove all below code before submitting to the judge system!
 // ------------------------------------------------------------
 
-(function() {
+(function () {
     var arr = [];
     if (typeof (require) == 'function') {
         // We are in node.js --> read the console input and process it
         require('readline').createInterface({
             input: process.stdin,
             output: process.stdout
-        }).on('line', function(line) {
+        }).on('line', function (line) {
             arr.push(line);
-        }).on('close', function() {
+        }).on('close', function () {
             console.log(processEstatesAgencyCommands(arr));
         });
     }
