48fdc5724d05120a001a17fb0ef4112dd9a76d6a
alex687 <alex.aramov@gmail.com>
2014-11-13 17:13:40 +0200

    KPK Exceptions Homework

---
+++ KPK/Assertions-and-Exceptions/Exceptions/CSharpExam.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class CSharpExam : Exam
+{
+    public CSharpExam(int score)
+    {
+        if (score < 0 || score > 100)
+        {
+            throw new ArgumentOutOfRangeException("score", "Score must be greater than zero and less than 100");
+        }
+
+        this.Score = score;
+    }
+
+    public int Score { get; private set; }
+
+    public override ExamResult Check()
+    {
+        return new ExamResult(this.Score, 0, 100, "Exam results calculated by score.");
+    }
+}
---
+++ KPK/Assertions-and-Exceptions/Exceptions/ExamResult.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class ExamResult
+{
+    public ExamResult(int grade, int minGrade, int maxGrade, string comments)
+    {
+        if (grade < 0)
+        {
+            throw new ArgumentOutOfRangeException("grade", "Grade must be greater than zero");
+        }
+
+        if (minGrade < 0)
+        {
+            throw new ArgumentOutOfRangeException("minGrade", "Min grade must be greater than zero");
+        }
+
+        if (maxGrade <= minGrade)
+        {
+            throw new ArgumentOutOfRangeException("maxGrade", "Max grade must be greater than minGrade");
+        }
+
+        if (string.IsNullOrEmpty(comments))
+        {
+            throw new ArgumentNullException("comments", "Comments cannot be null or empty");
+        }
+
+        this.Grade = grade;
+        this.MinGrade = minGrade;
+        this.MaxGrade = maxGrade;
+        this.Comments = comments;
+    }
+
+    public int Grade { get; private set; }
+
+    public int MinGrade { get; private set; }
+
+    public int MaxGrade { get; private set; }
+
+    public string Comments { get; private set; }
+}
