17038b528d0fbd10f6d7376a72a47a1e4fbb7c83
alex687 <alex_aramov@abv.bg>
2015-08-30 12:07:31 +0300

    Trees Homework

---
+++ DataStructures/Trees/FileTraversal/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace FileTraversal
+{
+    using System;
+    using System.IO;
+
+    public class Program
+    {
+        private const string StartFolder = "D:\\Just Cause 2";
+
+        public static void Main()
+        {
+            Folder root = new Folder(StartFolder);
+
+            RecursiveDirectorySearch(root);
+
+            // In bytes
+            Console.WriteLine(root.Size);
+        }
+
+        public static void RecursiveDirectorySearch(Folder folder)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(folder.Name);
+
+            foreach (FileInfo file in directoryInfo.GetFiles())
+            {
+                folder.Files.Add(new File(file.FullName, file.Length));
+            }
+
+            foreach (DirectoryInfo directory in directoryInfo.GetDirectories())
+            {
+                Folder newFolder = new Folder(directory.FullName);
+
+                folder.Folders.Add(newFolder);
+
+                RecursiveDirectorySearch(newFolder);
+            }
+        }
+    }
+}
---
+++ DataStructures/Trees/PlayWithTrees/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace PlayWithTrees
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int nodesCount = int.Parse(Console.ReadLine());
+
+
+            var nodes = new Dictionary<int, Tree>();
+            while (nodes.Count < nodesCount)
+            {
+                string input = Console.ReadLine();
+                var numbers = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(n => int.Parse(n))
+                    .ToArray();
+                var parrentNodeValue = numbers[0];
+                var childNodeValue = numbers[1];
+
+
+                CreateKeyIfNotExists(nodes, parrentNodeValue);
+                CreateKeyIfNotExists(nodes, childNodeValue);
+
+                var parrentNode = nodes[parrentNodeValue];
+                var childNode = nodes[childNodeValue];
+                parrentNode.Children.Add(childNode);
+                childNode.Parent = parrentNode;
+            }
+            var pathsSum = int.Parse(Console.ReadLine());
+            var subtreesSum = int.Parse(Console.ReadLine());
+
+            var rootNode = nodes.Where(n => n.Value.Parent == null);
+            if (rootNode.Count() != 1)
+            {
+                throw new InvalidOperationException("Invalid Tree.");
+            }
+
+            Tree resultTree = rootNode.First().Value;
+
+            Console.WriteLine("Root Node: {0}", resultTree.FindRootNode());
+            Console.WriteLine("Leaf Nodes: {0}",
+                string.Join(", ", resultTree.FindLeafNodes()));
+            Console.WriteLine("Middle Nodes: {0}",
+                string.Join(", ", resultTree.FindMiddleNodes()));
+            Console.WriteLine("Longest Path: {0}",
+                string.Join(" -> ", resultTree.FindLongestPath()));
+
+            IList<IList<int>> pathsWithValue = resultTree.FindPathsWithSum(pathsSum);
+            Console.WriteLine("Paths With Value {0}:", pathsSum);
+            foreach (var path in pathsWithValue)
+            {
+                Console.WriteLine(string.Join(" -> ", path));
+            }
+
+            IList<Tree> subtreesWithValue = resultTree.FindSubtreesWithSum(subtreesSum);
+            Console.WriteLine("Subtrees With Value {0}:", subtreesSum);
+            foreach (var tree in subtreesWithValue)
+            {
+                Console.WriteLine(tree);
+            }
+        }
+
+        private static void CreateKeyIfNotExists(Dictionary<int, Tree> nodes, int nodeValue)
+        {
+            if (!nodes.ContainsKey(nodeValue))
+            {
+                nodes.Add(nodeValue, new Tree(nodeValue));
+            }
+
+            return;
+        }
+    }
+}
